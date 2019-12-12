37e28b5296dc9a81873df29c3dde2a4814a2c252
nima2839 <nima2839@gmail.com>
2019-12-12 12:22:47 -0700

    Prepped the file to test T1 vector option

--- Test_Files/Test_All_GRASE.m
+++ Test_Files/Test_All_GRASE.m
@@ -1,12 +1,6 @@
 addpath(pwd)
-cd ~/Postprocessing
-addpath(genpath(pwd))
-cd ~
-addpath MWI/
+addpath(genpath('~/MWI'))
 names{1} = 'Can-05-Rrm-064-M0R';
-names{2} = 'Can-05-Rrm-065-M0R';
-names{3} = 'Can-05-Rrm-070-M0R';
-names{4} = 'Can-05-Rrm-082-M0R';
 
 for i = 1:length(names)
   ProcessGRASE(names{i});
