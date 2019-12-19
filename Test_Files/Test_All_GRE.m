9d0de66a358a310833bd0c195459272edf445299
nima2839 <nima2839@gmail.com>
2019-12-19 14:05:29 -0700

    High res from Javad

--- Test_Files/Test_All_GRE.m
+++ Test_Files/Test_All_GRE.m
@@ -3,9 +3,7 @@ addpath(genpath('~/MWI'))
 ReadPath = '~/GRE/GRE_To_Do/';
 SavePath = '~/GRE/GRE_Results/';
 
-names{1} = 'GRE_ROFlip_Brain.mat';
-names{2} = 'GRE_ROFlip_Brain_PhaseAP.mat';
-names{3} = 'GRE_ROFlip_Brain_RFSpoil.mat';
+names{1} = 'GRE_HighResJ.mat';
 
 for i = 1:length(names)
   ProcessGRE_Tukey(names{i}, ReadPath, SavePath);
