ec6f57e481a2ba9c3bd27620b16b1d339d342a9e
nima2839 <nima2893@gmail.com>
2019-12-22 20:13:18 -0700

    process all slices

--- Test_Files/FlipAngleGRASE_Test.m
+++ Test_Files/FlipAngleGRASE_Test.m
@@ -6,7 +6,7 @@ FA_Map(isnan(FA_Map)) = 0;
 FA_Map = medfilt3(FA_Map, [5 5 3]);
 
 tic;
-[maps,distributions,~] = T2map_Nima(tf_mgrase(:,:,15:16,:),'T1',T1, 'Threshold', 200, 'MinRefAngle', 60, 'nAngles', 12, 'T2Range', [0.015, 2],'FlipAngleMap', FA_Map(:,:,15:16,:));
+[maps,distributions,~] = T2map_Nima(tf_mgrase,'T1',T1, 'Threshold', 200, 'MinRefAngle', 60, 'nAngles', 12, 'T2Range', [0.015, 2],'FlipAngleMap', FA_Map);
 runtime = toc;
 Deccription = 'Input FA_Map from previous results,Threshold = 200; MinRefAngle= 60, nAngles = 10, T2Range = 1e-2 to 2, and T1 = 0.2,0.5,1, Cutoff = index 40';
 
