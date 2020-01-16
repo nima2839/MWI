ed07f8340fa8e697cc6e69d649d6a79ba235ae2e
nima2839 <nima2839@gmail.com>
2020-01-16 14:31:14 -0700

    Bug fixed!

--- Test_Files/Test_All_GRASE.m
+++ Test_Files/Test_All_GRASE.m
@@ -2,7 +2,7 @@ addpath(genpath('~/GRASE/Postprocessing'))
 addpath(genpath('~/MWI'))
 
 cd ~/GRASE/GRASE_To_Do/
-files = dir('*.*');
+files = dir('GRASE_Results_*.mat');
 
 for i = 1:length(files)
   ProcessGRASE(files(i).name);
