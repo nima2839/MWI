ca0fe4392e3af7cb12e5537d4ef0a638f4f27d3e
nima2839 <nima2839@gmail.com>
2020-02-19 12:19:01 -0700

    To test if adding a registered B1 map would amend the MWF maps! checking only for patient Can-05-Ppm-034-M0!

---
+++ Test_Files/UBC_Vs_BSB1.m
@@ -0,0 +1,34 @@
+addpath(genpath('~/GRASE/Postprocessing'))
+addpath(genpath('~/MWI'))
+
+cd ~/GRASE/GRASE_To_Do
+
+FileName = GRASE_Results_Can-05-Ppm-034-M0.mat;
+
+load(FileName, 'tf_mgrase')
+
+cd('~')
+FlipAngleMap = 180 * double(niftiread('rB1_Phase.nii')) / 800;
+
+[maps,distributions,~] = T2map_Nima(tf_mgrase, 'Threshold', 200, 'MinRefAngle', 60, 'nAngles', 12, 'T2Range', [0.015, 2], 'FlipAngleMap', FlipAngleMap);
+
+
+MWI = sqz(squeeze(sum(distributions(:,:,:,1:40),4))./squeeze(sum(distributions(:,:,:,:),4)));
+
+MWI_1 = ( ones( size( MWI )) - isnan( MWI ) ) ;
+[ Xres , Yres , Zres ] = size( MWI );
+Final_MWI = zeros( size( MWI ) ) ;
+for c = 1 : Zres
+    for a = 1 : Xres
+        for b = 1 : Yres
+            if MWI_1( a , b , c ) == 1
+                Final_MWI( a , b , c ) = MWI( a , b , c ) ;
+            end
+        end
+    end
+end
+
+runtime=toc;
+cd ~/GRASE/GRASE_Results
+Deccription = 'Threshold = 200; MinRefAngle= 60, nAngles = 12, FlipAngleMap from B1-map';
+save(['GRASE_Results_B1_map_', FileName])
