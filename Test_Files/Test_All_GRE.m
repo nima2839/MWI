4572c7e64c629fea9428e618861dd78e1a0af70b
nima2839 <nima2839@gmail.com>
2020-02-20 10:15:27 -0700

    To test the Feb20 dataset!

--- Test_Files/Test_All_GRE.m
+++ Test_Files/Test_All_GRE.m
@@ -3,7 +3,9 @@ addpath(genpath('~/MWI'))
 ReadPath = '~/GRE/GRE_To_Do/';
 SavePath = '~/GRE/GRE_Results/';
 
-names{1} = 'GRE_HighResJ.mat';
+names{1} = 'Feb20_GRE2D_25Cont_Bipolar.mat';
+names{2} = 'Feb20_GRE_20cont_Monopolar.mat';
+names{3} = 'Feb20_GRE_32cont.mat';
 
 for i = 1:length(names)
   ProcessGRE_Tukey(names{i}, ReadPath, SavePath);
