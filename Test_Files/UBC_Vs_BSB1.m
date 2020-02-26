dc99efa9c2f55220f638626732cfe74cd07cd08d
nima2839 <nima2839@gmail.com>
2020-02-26 11:48:29 -0700

    Testing subject 29!

--- Test_Files/UBC_Vs_BSB1.m
+++ Test_Files/UBC_Vs_BSB1.m
@@ -3,7 +3,7 @@ addpath(genpath('~/MWI'))
 
 cd ~/GRASE/GRASE_To_Do
 
-FileName = 'GRASE_Results_Can-05-Ppm-034-M0.mat';
+FileName = 'GRASE_Results_Can-05-Ris-029-M0.mat';
 
 load(FileName, 'tf_mgrase')
 
@@ -11,7 +11,7 @@ cd('~')
 FlipAngleMap = 180 * double(niftiread('rB1_Phase.nii')) / (800*1.165);
 FlipAngleMap = flip(permute(FlipAngleMap,[2 1 3]),1);
 tic
-[maps,distributions,~] = T2map_Nima(tf_mgrase, 'Threshold', 200, 'MinRefAngle', 60, 'nAngles', 12, 'T2Range', [0.015, 2], 'FlipAngleMap', FlipAngleMap);
+[maps,distributions,~] = T2map_Nima(tf_mgrase, 'Threshold', 200, 'T2Range', [0.015, 2], 'FlipAngleMap', FlipAngleMap);
 
 
 MWI = sqz(squeeze(sum(distributions(:,:,:,1:40),4))./squeeze(sum(distributions(:,:,:,:),4)));
@@ -31,5 +31,5 @@ end
 
 runtime=toc;
 cd ~/GRASE/GRASE_Results
-Deccription = 'Threshold = 200; MinRefAngle= 60, nAngles = 12, FlipAngleMap from B1-map with factor 1.165';
+Description = 'Threshold = 200; MinRefAngle= 60, nAngles = 12, FlipAngleMap from B1-map with factor 1.165';
 save(['GRASE_Results_B1_map_', FileName])
