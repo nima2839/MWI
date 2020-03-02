0034f812adeb8d85baa274af478b0383d092ed70
nima2839 <nima2839@gmail.com>
2020-03-02 11:52:25 -0700

    There was a problem with registration of b1 map to GRASE data in subject 18!

--- Test_Files/Test_All_B1Maps.m
+++ Test_Files/Test_All_B1Maps.m
@@ -4,7 +4,7 @@ addpath(genpath('~/MWI'))
 names{1} = 'Can-05-Rrm-005-M0';
 names{2} = 'Can-05-Rrm-011-M0';
 names{3} = 'Can-05-Rrm-013-M0';
-names{4} = 'Can-05-Rrm-018-M0';
+names{4} = 'Can-05-Rrm-082-M0R';
 names{5} = 'Can-05-Rrm-019-M0';
 names{6} = 'Can-05-Rrm-023-M0';
 names{7} = 'Can-05-Rrm-025-M0';
@@ -16,7 +16,7 @@ names{12} = 'Can-05-Rrm-057-M0';
 names{13} = 'Can-05-Rrm-064-M0R';
 names{14} = 'Can-05-Rrm-065-M0R';
 names{15} = 'Can-05-Rrm-070-M0R';
-names{16} = 'Can-05-Rrm-082-M0R';
+%names{16} = 'Can-05-Rrm-018-M0';
 
 for i = 1:length(names)
   UBC_Vs_BSB1(names{i});
