88a103a0bdf271a566cd69c26b40e68a1d46bfd8
nima2839 <nima2839@gmail.com>
2020-03-02 10:40:09 -0700

    Testing B1_map input for all subjects!

---
+++ Test_Files/Test_All_B1Maps.m
@@ -0,0 +1,23 @@
+addpath(genpath('~/GRASE/Postprocessing'))
+addpath(genpath('~/MWI'))
+
+names{1} = 'Can-05-Rrm-005-M0';
+names{2} = 'Can-05-Rrm-011-M0';
+names{3} = 'Can-05-Rrm-013-M0';
+names{4} = 'Can-05-Rrm-018-M0';
+names{5} = 'Can-05-Rrm-019-M0';
+names{6} = 'Can-05-Rrm-023-M0';
+names{7} = 'Can-05-Rrm-025-M0';
+names{8} = 'Can-05-Rrm-031-M0';
+names{9} = 'Can-05-Rrm-043-M0';
+names{10} = 'Can-05-Rrm-053-M0';
+names{11} = 'Can-05-Rrm-054-M0';
+names{12} = 'Can-05-Rrm-057-M0';
+names{13} = 'Can-05-Rrm-064-M0R';
+names{14} = 'Can-05-Rrm-065-M0R';
+names{15} = 'Can-05-Rrm-070-M0R';
+names{16} = 'Can-05-Rrm-082-M0R';
+
+for i = 1:length(names)
+  UBC_Vs_BSB1(names{i});
+end
