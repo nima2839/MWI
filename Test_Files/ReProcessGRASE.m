6c4cfb5035803b443e41b5e2edc761cc83a27cda
nima2839 <nima2893@gmail.com>
2020-03-18 20:34:51 -0600

    To reprocess all the GRASE data with nT2 = 60!

---
+++ Test_Files/ReProcessGRASE.m
@@ -0,0 +1,26 @@
+function ReProcessGRASE(name)
+load(name,'tf_mgrase')
+tic
+
+[maps,distributions,~] = T2map_SEcorr(tf_mgrase, 'Threshold', 200,'nT2', 60);
+
+
+MWI = squeeze(squeeze(sum(distributions(:,:,:,1:40),4))./squeeze(sum(distributions(:,:,:,:),4)));
+
+MWI_1 = ( ones( size( MWI )) - isnan( MWI ) ) ;
+[ Xres , Yres , Zres ] = size( MWI );
+Final_MWI = zeros( size( MWI ) ) ;
+for c = 1 : Zres
+    for a = 1 : Xres
+        for b = 1 : Yres
+            if MWI_1( a , b , c ) == 1
+                Final_MWI( a , b , c ) = MWI( a , b , c ) ;
+            end
+        end
+    end
+end
+clear MWI MWI_1 tf_mgrase
+runtime=toc;
+Description = 'Threshold = 200; nT2 = 60 ';
+save(name)
+end
--- Test_Files/Test_All_GRASE.m
+++ Test_Files/Test_All_GRASE.m
@@ -1,23 +1,13 @@
 addpath(genpath('~/GRASE/Postprocessing'))
 addpath(genpath('~/MWI'))
 
-%cd ~/GRASE/GRASE_To_Do/
-%files = dir('GRASE_Results_*.mat');
+cd ~/GRASE/GRASE_Results/GRASE_UBC_Results
+files = dir('*.mat');
 
-name{1} = 'Can-05-Ppm-067-M0R';
-name{2} = 'Can-05-Ppm-076-M0R';
-name{3} = 'Can-05-Ppm-078-M0R';
-name{4} = 'Can-05-Rrm-087-M0R';
-name{5} = 'Can-05-Ppm-088-M0R';
-name{6} = 'Can-05-Rrm-089-M0R';
-name{7} = 'Can-05-Ris-095-M0R';
-name{8} = 'Can-05-Rrm-086-M0R';
-name{9} = 'Can-05-Rrm-094-M0R';
-name{10} = 'Can-05-Ris-105-M0R';
-name{11} = 'Can-05-Ppm-107-M0R';
-name{12} = 'Can-05-Rrm-113-M0R';
+%name{1} = 'Can-05-Ppm-067-M0R';
 
-for i = 1:length(name)
+
+for i = 1:length(files)
   %ProcessGRASE(files(i).name);
-  ProcessGRASE(name{i});
+  ReProcessGRASE(files(i).name);
 end
