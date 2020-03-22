9c819a325d84273548aed657b02d5fc4ec8f9700
nima2839 <nima2839@gmail.com>
2020-03-22 11:57:56 -0600

    Testing with nT2 = 60!

--- Test_Files/UBC_Vs_BSB1.m
+++ Test_Files/UBC_Vs_BSB1.m
@@ -1,8 +1,6 @@
 function UBC_Vs_BSB1(Subject)
   cd ~/GRASE/GRASE_Results
 
-  FileName = ['GRASE_Results_B1_map_',Subject,'.mat'];
-
   load(FileName, 'tf_mgrase')
 
   cd(['~/GRASE/B1_Maps/',Subject,'/'])
@@ -10,10 +8,10 @@ function UBC_Vs_BSB1(Subject)
   FlipAngleMap = (159.5 * double(niftiread('rB1_Phase.nii'))) / (800);
   FlipAngleMap = flip(permute(FlipAngleMap,[2 1 3]),1);
   tic
-  [maps,distributions,~] = T2map_Nima(tf_mgrase, 'Threshold', 200, 'T2Range', [0.015, 2], 'FlipAngleMap', FlipAngleMap);
+  [maps,distributions,~] = T2map_Nima(tf_mgrase, 'Threshold', 200, 'T2Range', [0.015, 2], 'FlipAngleMap', FlipAngleMap,'nT2', 60);
 
 
-  MWI = squeeze(squeeze(sum(distributions(:,:,:,1:40),4))./squeeze(sum(distributions(:,:,:,:),4)));
+  MWI = squeeze(squeeze(sum(distributions(:,:,:,1:13),4))./squeeze(sum(distributions(:,:,:,:),4)));
 
   MWI_1 = ( ones( size( MWI )) - isnan( MWI ) ) ;
   [ Xres , Yres , Zres ] = size( MWI );
@@ -29,8 +27,11 @@ function UBC_Vs_BSB1(Subject)
   end
 
   runtime=toc;
+
+  clear tf_mgrase MWI MWI_1 FlipAngleMap
+
   cd ~/GRASE/GRASE_Results
-  Description = 'Threshold = 200;FlipAngleMap from B1-map with nominal angle of 159.5';
+  Description = 'Threshold = 200;FlipAngleMap from B1-map with nominal angle of 159.5, nT2 = 60';
   save(['GRASE_Results_B1_map_', Subject])
 
 end
