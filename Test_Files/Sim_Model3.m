76fe008c4fc6d8d0a863dabcd5be846b8b76e899
nima2839 <nima2893@gmail.com>
2020-04-16 17:27:28 -0600

    Simulation files to test efficency of GRASE processing with and without the actual values!

---
+++ Test_Files/Sim_Model3.m
@@ -0,0 +1,49 @@
+addpath(genpath('~/MWI'))
+addpath(genpath('~/GRASE/Postprocessing'))
+
+cd ~/Simulation/B1_Research/
+load('Models') % predefined models
+
+
+MyInfo.Times = (1:32)*1e-2;
+
+MyInfo.T2Dist.T2Values = time;
+MyInfo.T2Dist.Weights = M3;
+
+MyInfo.T1Val = ones(1,length(M1));
+MyInfo.T1Val(1:15) = 0.5;
+MyInfo.T1Val(33:end) = 4.163;
+MyInfo.FlipAngle = 180;
+MyInfo.NumData = 500;
+MyInfo.TrueFAFlag = false;
+MyInfo.SNR = 0;
+
+FA = 120:180;
+SNR = [40:2:60, 80];
+nFA = length(FA);
+nSNR = length(SNR);
+
+parfor j = 1:nSNR
+	temp = MyInfo;
+	temp.SNR = SNR(j);
+	temp_dist = cell(nFA);
+	temp_maps = cell(nFA);
+	temp_Tdist = cell(nFA);
+	temp_Tmaps = cell(nFA);
+	for i  = 1:nFA
+		temp.FlipAngle = FA(i)
+		temp.TrueFAFlag = false;
+		a = SimClass(temp);
+		[temp_dist{i},temp_maps{i}] = UBC_Fitting(a);
+		a.MyInfo.TrueFAFlag = true;
+		[temp_Tdist{i},temp_Tmaps{i}] = UBC_Fitting(a);
+	end
+	Dist{j}{:} = temp_dist;
+	Maps{j}{:} = temp_maps;
+	TrueFA_Dist{j}{:} = temp_Tdist;
+	TrueFA_Maps{j}{:}= temp_Tmaps;
+end
+clear a temp
+Description = 'second dim is FlipAngle, and first dim is SNR';
+cd ~/Simulation/B1_Research/
+save('B1_Sim_Result_M3','-v7.3')
