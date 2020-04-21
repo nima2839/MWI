a196b16803c5c149037e961d3126b7f4d9eb2ca1
nima2839 <nima2893@gmail.com>
2020-04-21 12:19:21 -0600

    removed the Nima process of the code!

--- Test_Files/ProcessGRASE.m
+++ Test_Files/ProcessGRASE.m
@@ -1,5 +1,5 @@
 function  ProcessGRASE( FileName )
-% function: basic process of GRASE MWI given by UBC
+% function: basic process of GRASE MWI using the code given by UBC
 tic;
 
 
@@ -38,35 +38,9 @@ for c = 1 : Zres
     end
 end
 
-clear MWI MWI_1 mgrase hfilt2
+clear MWI MWI_1 mgrase hfilt2 tf_mgrase
 runtime=toc;
 cd ~/GRASE/GRASE_Results
 Description = 'Threshold = 200; nT2 = 60, T2Range = 8ms to 2s, MinRefAngle = 100 degrees ';
 save(['GRASE_Results_', FileName])
-
-tic
-
-[maps,distributions,~] = T2map_Nima(tf_mgrase, 'Threshold', 200,'nT2', 60,'T2Range', [0.008, 2], 'MinRefAngle', 100);
-
-
-MWI = squeeze(squeeze(sum(distributions(:,:,:,1:18),4))./squeeze(sum(distributions(:,:,:,:),4)));
-
-MWI_1 = ( ones( size( MWI )) - isnan( MWI ) ) ;
-[ Xres , Yres , Zres ] = size( MWI );
-Final_MWI = zeros( size( MWI ) ) ;
-for c = 1 : Zres
-    for a = 1 : Xres
-        for b = 1 : Yres
-            if MWI_1( a , b , c ) == 1
-                Final_MWI( a , b , c ) = MWI( a , b , c ) ;
-            end
-        end
-    end
-end
-
-clear MWI MWI_1 
-runtime=toc;
-cd ~/GRASE/GRASE_Results
-Description = 'Threshold = 200; nT2 = 60, T2Range = 8ms to 2s, MinRefAngle = 100 degrees ';
-save(['GRASE_Results_Extended_B1_', FileName])
 end
