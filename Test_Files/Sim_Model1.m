5217481ee403945c24387ca7fd352e655c3d920c
nima2839 <nima2893@gmail.com>
2020-04-21 14:31:31 -0600

    Updated!

--- Test_Files/Sim_Model1.m
+++ Test_Files/Sim_Model1.m
@@ -4,55 +4,8 @@ addpath(genpath('~/GRASE/Postprocessing'))
 cd ~/Simulation/B1_Research/
 load('Models') % predefined models
 
-
+%%
 MyInfo.Times = (1:32)*1e-2;
-
-MyInfo.T2Dist.T2Values = time;
-MyInfo.T2Dist.Weights = M2;
-
-MyInfo.T1Val = ones(1,length(M1));
-MyInfo.T1Val(1:15) = 0.5;
-MyInfo.T1Val(33:end) = 2.5;
-MyInfo.FlipAngle = 180;
-MyInfo.NumData = 500;
-MyInfo.TrueFAFlag = false;
-MyInfo.SNR = 0;
-
-FA = 120:180;
-SNR = [40:2:60, 80];
-nFA = length(FA);
-nSNR = length(SNR);
-
-parfor j = 1:nSNR
-	temp = MyInfo;
-	temp.SNR = SNR(j);
-	temp_dist = cell(nFA);
-	temp_maps = cell(nFA);
-	temp_Tdist = cell(nFA);
-	temp_Tmaps = cell(nFA);
-	for i  = 1:nFA
-		temp.FlipAngle = FA(i)
-		temp.TrueFAFlag = false;
-		a = SimClass(temp);
-		[temp_dist{i},temp_maps{i}] = UBC_Fitting(a);
-		a.MyInfo.TrueFAFlag = true;
-		[temp_Tdist{i},temp_Tmaps{i}] = UBC_Fitting(a);
-	end
-	Dist{j}{:} = temp_dist;
-	Maps{j}{:} = temp_maps;
-	TrueFA_Dist{j}{:} = temp_Tdist;
-	TrueFA_Maps{j}{:}= temp_Tmaps;
-end
-clear a temp
-Description = 'second dim is FlipAngle, and first dim is SNR';
-cd ~/Simulation/B1_Research/
-save('B1_Sim_Result_M2','-v7.3')
-
-addpath(genpath('~/MWI'))
-addpath(genpath('~/GRASE/Postprocessing'))
-
-MyInfo.Times = (1:32)*1e-2;
-load('Models')
 
 MyInfo.T2Dist.T2Values = time;
 MyInfo.T2Dist.Weights = M1;
@@ -61,36 +14,41 @@ MyInfo.T1Val = ones(1,length(M1));
 MyInfo.T1Val(1:15) = 0.5;
 
 MyInfo.FlipAngle = 180;
-MyInfo.NumData = 500;
+MyInfo.NumData = qe4;
 MyInfo.TrueFAFlag = false;
 MyInfo.SNR = 0;
-
-FA = 120:180;
-SNR = [40:2:60, 80];
+%%
+FA = 130:180;
+SNR = [40:2:60, 100];
 nFA = length(FA);
 nSNR = length(SNR);
-
+Dist =  cell(nSNR,nFA);
+Maps = Dist;
+TrueFA_Dist = Dist;
+TrueFA_Maps = Dist;
 parfor j = 1:nSNR
+    disp(j)
 	temp = MyInfo;
 	temp.SNR = SNR(j);
-	temp_dist = cell(nFA);
-	temp_maps = cell(nFA);
-	temp_Tdist = cell(nFA);
-	temp_Tmaps = cell(nFA);
+	temp_dist = cell(1,nFA);
+	temp_maps = cell(1,nFA);
+	temp_Tdist = cell(1,nFA);
+	temp_Tmaps = cell(1,nFA);
 	for i  = 1:nFA
-		temp.FlipAngle = FA(i)
+		temp.FlipAngle = FA(i);
 		temp.TrueFAFlag = false;
 		a = SimClass(temp);
-		[temp_dist{i},temp_maps{i}] = UBC_Fitting(a);
+		[temp_dist{1,i}, temp_maps{1,i}] = UBC_Nima_Fitting(a);
 		a.MyInfo.TrueFAFlag = true;
-		[temp_Tdist{i},temp_Tmaps{i}] = UBC_Fitting(a);
+		[temp_Tdist{1,i}, temp_Tmaps{1,i}] = UBC_Nima_Fitting(a);
 	end
-	Dist{j}{:} = temp_dist;
-	Maps{j}{:} = temp_maps;
-	TrueFA_Dist{j}{:} = temp_Tdist;
-	TrueFA_Maps{j}{:}= temp_Tmaps;
+	Dist(j,:) = temp_dist;
+	Maps(j,:) = temp_maps;
+	TrueFA_Dist(j,:) = temp_Tdist;
+	TrueFA_Maps(j,:)= temp_Tmaps;
 end
 clear a temp
 Description = 'second dim is FlipAngle, and first dim is SNR';
+%%
 cd ~/Simulation/
 save('B1_Sim_Result_M1','-v7.3')
