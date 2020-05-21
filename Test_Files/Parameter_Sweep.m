3263c1cab31e41a2ebae804ff2bf68da8cf0d72b
nima2839 <nima2893@gmail.com>
2020-05-21 14:08:28 -0600

    Created to test a variety of MWFs and Chi2Factors!

---
+++ Test_Files/Parameter_Sweep.m
@@ -0,0 +1,22 @@
+% this is a parametr sweep code for B1 effect research
+addpath(genpath('~/MWI'))
+addpath(genpath('~/GRASE/Postprocessing'))
+
+Chi2Factors = [1.005, 1.010, 1.015, 1.02];
+MWFs = (1:2:21) / 100;
+
+Models = cell(length(Chi2Factors), length(MWFs));
+
+tic
+
+for i = 1:length(Chi2Factors)
+	for j = 1:length(MWFs)
+		Models{i,j} = Template_Model(MWFs(j), Chi2Factors(i));
+		disp(strcat(string((i+j)/ (length(Chi2Factors) + length(MWFs))) + "% ..."))
+	end
+end
+
+runtime = toc;
+
+save('B1_Sim_Result_M5','-v7.3')
+disp('All Done!')
---
+++ Test_Files/Template_Model.m
@@ -0,0 +1,66 @@
+function out = Template_Model(MWF, Chi2Factor, TimeConstRange)
+% to use a template for simulation results of SimClass
+
+CSF = 0.1; % Fraction of CSF
+
+IE = 1 - MWF - CSF; % Fraction of intra-extra cellular water
+
+MyInfo.NumWaterComp = 3;
+MyInfo.Times = (1:32)*1e-2;
+if nargin < 3
+	MyInfo.TimeConstRange{1} = [3 10]*1e-3;
+	MyInfo.TimeConstRange{2} = [70 80]*1e-3;
+	MyInfo.TimeConstRange{3} = [500 2000]*1e-3;
+else
+	MyInfo.TimeConstRange = TimeConstRange
+end
+MyInfo.T1Val = [.6 1 4.163];
+MyInfo.FractionRange{1}= [MWF, MWF];
+MyInfo.FractionRange{2}= [IE, IE];
+MyInfo.FractionRange{3}= [CSF, CSF];
+
+
+
+MyInfo.FlipAngle = 180;
+MyInfo.NumData = 500;
+MyInfo.TrueFAFlag = false;
+MyInfo.SNR = 0;
+%%
+FA = 110:180;
+SNR = [50,100:50:300, 500, 750, 1e3];
+nFA = length(FA);
+nSNR = length(SNR);
+Dist =  cell(nSNR,nFA);
+Maps = Dist;
+TrueFA_Dist = Dist;
+TrueFA_Maps = Dist;
+parfor j = 1:nSNR
+	temp = MyInfo;
+	temp.SNR = SNR(j);
+	temp_dist = cell(1,nFA);
+	temp_maps = cell(1,nFA);
+	temp_Tdist = cell(1,nFA);
+	temp_Tmaps = cell(1,nFA);
+	for i  = 1:nFA
+		temp.FlipAngle = FA(i);
+		temp.TrueFAFlag = false;
+		a = SimClass(temp);
+		[temp_dist{1,i}, temp_maps{1,i}] = UBC_Nima_Fitting(a);
+		a.MyInfo.TrueFAFlag = true;
+		[temp_Tdist{1,i}, temp_Tmaps{1,i}] = UBC_Nima_Fitting(a, ones(1,60), Chi2Factor);
+	end
+	Dist(j,:) = temp_dist;
+	Maps(j,:) = temp_maps;
+	TrueFA_Dist(j,:) = temp_Tdist;
+	TrueFA_Maps(j,:)= temp_Tmaps;
+	disp(j)
+end
+
+out.MyInfo = MyInfo;
+out.FA = FA;
+out.SNR = SNR;
+out.Dist = Dist;
+out.Maps = Maps;
+out.TrueFA_Dist = TrueFA_Dist;
+out.TrueFA_Maps = TrueFA_Maps;
+end
