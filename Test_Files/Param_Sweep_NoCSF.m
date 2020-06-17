87588fa8579a4f718dbeb08ae70a74447435fc4e
nima2839 <nima2893@gmail.com>
2020-06-17 12:39:59 -0600

    Calculation of MWF = 0 estimates!

--- Test_Files/Param_Sweep_NoCSF.m
+++ Test_Files/Param_Sweep_NoCSF.m
@@ -4,19 +4,19 @@ addpath(genpath('~/GRASE/Postprocessing'))
 clear
 clc
 
-Chi2Factors = 1.01;
-MWFs = (1:3:30) / 100;
+Chi2Factors = 1.02;
+MWFs = 0;
 
 
 
-MyInfo.NumWaterComp = 2;
+MyInfo.NumWaterComp = 1;
 MyInfo.Times = (1:32)*1e-2;
-MyInfo.TimeConstRange{1} = [5 20]*1e-3;
-MyInfo.TimeConstRange{2} = [70 80]*1e-3;
-MyInfo.T1Val = [.6 1];
+%MyInfo.TimeConstRange{1} = [5 20]*1e-3;
+MyInfo.TimeConstRange{1} = [70 80]*1e-3;
+MyInfo.T1Val = [1];
 
 MyInfo.FlipAngle = 180;
-MyInfo.NumData = 500;
+MyInfo.NumData = 5e3;
 MyInfo.TrueFAFlag = false;
 MyInfo.SNR = 0;
 
@@ -30,8 +30,8 @@ for i = 1:length(Chi2Factors)
 	MyInfo.Chi2Factor = Chi2Factors(i);
 	for j = 1:length(MWFs)
 		IE = 1 - MWFs(j);
-		MyInfo.FractionRange{1}= [MWFs(j), MWFs(j)];
-		MyInfo.FractionRange{2}= [IE, IE];
+		%MyInfo.FractionRange{1}= [MWFs(j), MWFs(j)];
+		MyInfo.FractionRange{1}= [IE, IE];
 		Models{i,j} = Template_Model(MyInfo);
 		disp(strcat(string(100*(i+j)/ (length(Chi2Factors) + length(MWFs))) + '% ...'))
 	end
@@ -39,6 +39,6 @@ end
 
 runtime = toc;
 cd ~/Simulation/B1_Research/
-save('B1_Sim_Result_PS_AltChi2_NoCSF','-v7.3')
+save('B1_Sim_Result_PS_NoMWF_NoCSF','-v7.3')
 disp('All Done!')
 clear
