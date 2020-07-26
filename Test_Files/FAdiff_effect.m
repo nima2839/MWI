f297f4de9b56e23fbe09174f6118316e1d5ef882
nima2839 <nima2893@gmail.com>
2020-07-26 04:01:19 -0600

    Checking distributions at larger FA_diff to investigate MWF_est = 0!

--- Test_Files/FAdiff_effect.m
+++ Test_Files/FAdiff_effect.m
@@ -20,10 +20,10 @@ MyInfo.T1Val = [.6 1 4.163];
 MyInfo.FlipAngle = 180;
 MyInfo.NumData = 500;
 MyInfo.TrueFAFlag = true;
-MyInfo.SNR = 1e2;
+MyInfo.SNR = 1e4;
 
-FA = [110:1:160, 165:5:180];
-FAdiff = -20:0.5:20;
+FA = [110:10:160];%, 165:5:180];
+FAdiff = +5:0.5:20;
 
 Results = cell(length(FA), length(FAdiff));
 
@@ -38,10 +38,11 @@ for i = 1:length(FA)
 	SimulatedData = TempSim.SimulatedData + Noise;
 	for j = 1:length(FAdiff)
 		MyInfo.FlipAngle = FA(i) + FAdiff(j);
-		[~, TempMaps] = SimClass.UBC_Nima_Fitting(SimulatedData, MyInfo);
+		[Dist, TempMaps] = SimClass.UBC_Nima_Fitting(SimulatedData, MyInfo);
 		Maps.MWF = mean(TempMaps.MWF(:));
 		Maps.E2_E1 = mean(TempMaps.E2_E1(:));
 		Maps.T_E2_E1 = T_E2_E1;
+		Maps.Dist = Dist;
 		Results{i,j} = Maps;
 	end
 	disp(strcat(string(100*i/length(FA)),'%'));
@@ -49,7 +50,7 @@ end
 toc
 
 cd ~/Simulation/B1_Research/
-save('FAdiff_Effect_Results_NoCSF_100SNR','Results','FA','FAdiff','MyInfo')
+save('FAdiff_Effect_Results_NoCSF_Dist','Results','FA','FAdiff','MyInfo')
 
 
 
