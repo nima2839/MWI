1f25c1f11e96f43b9d45cf58191a60e94820277e
nima2839 <nima2893@gmail.com>
2020-08-22 16:13:28 -0600

    Extra data are cleared to save storage space!

--- Test_Files/GRE_20_192.m
+++ Test_Files/GRE_20_192.m
@@ -24,15 +24,30 @@ test = Calc_SC(test,2); % LOG method
 %test = Calc_3PM(test);
 X0 = [0.1,   300,	  5,	0.6,	15,	0.3,	25,	   0];
 test1 = Calc_3PM(test);
+MWF{1} = test1.MWF_3PM;
+Res{1} = test1.Res_3PM;
+clear test1
 X0 = [0.1,   100,	  5,	0.6,	15,	0.3,	25,	   0];
 test2 = Calc_3PM(test);
+MWF{2} = test2.MWF_3PM;
+Res{2} = test2.Res_3PM;
+clear test2
 X0 = [0.1,   80,	  5,	0.6,	15,	0.3,	25,	   0];
 test3 = Calc_3PM(test);
+MWF{3} = test3.MWF_3PM;
+Res{3} = test3.Res_3PM;
+clear test3
 X0 = [0.1,   60,	  5,	0.6,	15,	0.3,	25,	   0];
 test4 = Calc_3PM(test);
+MWF{4} = test4.MWF_3PM;
+Res{4} = test4.Res_3PM;
+clear test4
 disp('Saving results...')
 test.Description = 'Calculating 8Param C3PM! LFGC!Tukey alpha = 0.25!4seed test!';
-test.RunTime = toc;
+RunTime = toc;
+Mag = test.Mag;
+LFGC = test.LFGC;
+clear test filtered
 cd ~/GRE/GRE_Results/
 save('20Cont_192_Monopolar_Tukey_MultiSeed');
 disp('Done!')
