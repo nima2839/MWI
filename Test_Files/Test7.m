da153cc5eb87019b38a136aa985978a8887c9aa8
nima2839 <nima2893@gmail.com>
2020-08-25 12:13:14 -0600

    NESMA Threshold = 0.05 and without normalization!

--- Test_Files/Test7.m
+++ Test_Files/Test7.m
@@ -1,30 +1,30 @@
-load Gre2D_18Con_Monopolar.mat
+cd ~/GRE/GRE_To_Do/
+load NMT3_2DGRE_18Cont_Monopolar.mat
 
-myinfo.Mask = Mask;
-myinfo.Method = 1;	% Du method
-myinfo.FirstTE = 2.01e-3;
-myinfo.EchoSpacing = 2.87e-3;
-myinfo.Vox = [1.5 1.5 3] * 1e-3;
+
 
 sd = size(Mag);
-K = Tukey3D(sd(1),sd(2),sd(3),0.5);
-complex_data = Mag(:,:,:,:).*exp(1i*Phase(:,:,:,:));
+K = Tukey3D(sd(1),sd(2),sd(3),0.3);
+complex_data = Mag.*exp(1i*Phase);
 clear Mag Phase
 
 for i = 1:sd(4)
-	filtered(:,:,:,i) = Mask.*ifftn(fftshift(fftshift(fftn(complex_data(:,:,:,i))).*K)) ;
+	filtered(:,:,:,i) =  Info.Mask.*ifftn(fftshift(fftshift(fftn(complex_data(:,:,:,i))).*K)) ;
 end
-%filtered = complex_data;
+
 tic
-test = TestClass(abs(filtered),angle(filtered),myinfo);
+test = TestClass(NESMA_Filter(abs(filtered),Info.Mask, 5,0.05),angle(filtered),Info);
 test = CalcLFGC(test);
-test = Calc_SC(test,2); % LOG method
-%test = Calc_2PM(test);
-%test = Calc_3PM(test);
-test = Calc_Complex3PM(test);
+
+
+X0 = [0.1,   60,	  5,	0.6,	15,	0.3,	25,	   0];
+test = Calc_3PM(test);
+
+
 disp('Saving results...')
-test.Description = 'Calculating 8Param complex 3PM ! LFGC!!';
-test.RunTime = toc;
-data = GetAllData(test);
-save('18Cont_Monopolar_Tukey_C3PM','data');
+test.Description = 'Calculating 8Param 3PM! LFGC!Tukey alpha = 0.3! NESMA without Normalizing!';
+RunTime = toc;
+
+cd ~/GRE/GRE_Results/
+save('18Cont_2DMonopolar_NESMA_nonNormolized');
 disp('Done!')
