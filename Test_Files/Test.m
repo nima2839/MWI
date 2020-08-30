d038e1ea0017fdcf7cac5f486459aee40337f954
nima2839 <nima2893@gmail.com>
2020-08-30 15:42:06 -0600

    No LFGC

--- Test_Files/Test.m
+++ Test_Files/Test.m
@@ -9,16 +9,15 @@ complex_data = Mag.*exp(1i*Phase);
 clear Mag Phase
 
 for i = 1:sd(4)
-	filtered(:,:,:,i) =  Info.Mask.*ifftn(fftshift(fftshift(fftn((Mag_Bias.^-2).*complex_data(:,:,:,i))).*K)) ;
+	filtered(:,:,:,i) =  Info.Mask.*ifftn(fftshift(fftshift(fftn(complex_data(:,:,:,i))).*K))./Mag_Bias ;
 end
 
 tic
 
 test = TestClass(abs(filtered),angle(filtered),Info);
-test = CalcLFGC(test);
-idx = 15:17;
-test.LFGC(:,:,idx,:) = NESMA_Filter(test.LFGC(:,:,idx,:),Info.Mask(:,:,idx), false, 0.05);
-
+idx = 13:18;
+test.Mag(:,:,idx,:) = NESMA_Filter(test.Mag(:,:,idx,:),Info.Mask(:,:,idx),false, 0.05);
+%test = CalcLFGC(test);
 
 %test2 = Calc_Multi_Seed(test);
 test1 = Calc_3PM(test);
