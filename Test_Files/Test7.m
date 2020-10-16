5f0287940f8dab3d42577aa0893f7f2a85833fba
nima2839 <nima2893@gmail.com>
2020-10-16 12:26:33 -0600

    removed init data

--- Test_Files/Test7.m
+++ Test_Files/Test7.m
@@ -22,8 +22,7 @@ test = TestClass(abs(filtered(:,:,14:16,:)),angle(filtered(:,:,14:16,:)),myinfo)
 test = CalcLFGC(test);
 test.LFGC = NESMA_Filter(test.LFGC,Info.Mask(:,:,14:16),true, 0.02)
 
-X0 = [0.1,   100,	  5,	0.6,	15,	0.3,	25,	   0];
-test = Calc_3PM(test,X0);
+test = Calc_3PM(test);
 
 
 disp('Saving results...')
