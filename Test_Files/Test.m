d8bdd2dcd8ad04799b89cf6b637f33a87c59e14e
nima2839 <nima2893@gmail.com>
2020-10-20 16:53:25 -0600

    using slices 14:16

--- Test_Files/Test.m
+++ Test_Files/Test.m
@@ -4,29 +4,34 @@ load NMT3_2DGRE_18Cont_Monopolar.mat
 
 
 sd = size(Mag);
-K = repmat(Tukey3D(sd(1),sd(2),1,0.25),[1,1,sd(3)]);
+K = repmat(Tukey3D(sd(1),sd(2),1,0.35),[1,1,sd(3)]);
 complex_data = Mag.*exp(1i*Phase);
 clear Mag Phase
 
 for i = 1:sd(4)
-	filtered(:,:,:,i) =  Info.Mask.*ifftn(fftshift(fftshift(fftn(complex_data(:,:,:,i)./Mag_Bias)).*K)) ;
+	filtered(:,:,:,i) =  Info.Mask.*ifftn(fftshift(fftshift(fftn(complex_data(:,:,:,i))).*K))./Mag_Bias ;
 end
 
 tic
 
 test = TestClass(abs(filtered),angle(filtered),Info);
 test = CalcLFGC(test);
-idx = 5:25;
-test.LFGC(:,:,idx,:) = NESMA_Filter(test.LFGC(:,:,idx,:),Info.Mask(:,:,idx),false, 0.01);
 
-test2 = Calc_Multi_Seed(test);
-test1 = Calc_3PM(test);
+
+idx = 14:16;
+
+opt.Mask = Info.Mask(:,:,idx);
+test.LFGC(:,:,idx,:) = NESMA_Filter(test.LFGC(:,:,idx,:),opt);
+
+test = Calc_3PM(test);
 
 disp('Saving results...')
-test.Description = 'Calculating 8Param 3PM! LFGC!Tukey alpha = 0.3';
+test.Description = 'Calculating 8Param 3PM! LFGC!Tukey alpha = 0.35';
 RunTime = toc;
-MWF = test1.MWF_3PM;
-MWF_MS = test2.MWF_3PM;
+
+MWF = test.MWF_3PM;
+LFGC = test.LFGC
+
 cd ~/GRE/GRE_Results/
-save('18Cont_2DMonopolar_NESMA_MS_2','MWF', 'MWF_MS','test1','test2');
+save('18Cont_2DMonopolar_NESMA','MWF','LFGC');
 disp('Done!')
