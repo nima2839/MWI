b2f5be92fffc7baee758c1579dcea6408c501397
nima2839 <nima2893@gmail.com>
2020-11-16 19:02:15 -0700

    removing bias before lowpass filtering process!

--- Test_Files/Test8.m
+++ Test_Files/Test8.m
@@ -9,7 +9,7 @@ complex_data = Mag.*exp(1i*Phase);
 clear Mag Phase
 
 for i = 1:sd(4)
-	filtered(:,:,:,i) =  Info.Mask.*ifftn(fftshift(fftshift(fftn(complex_data(:,:,:,i))).*K))./Mag_Bias ;
+	filtered(:,:,:,i) =  Info.Mask.*ifftn(fftshift(fftshift(fftn(complex_data(:,:,:,i)./Mag_Bias)).*K)) ;
 end
 clear complex_data Mag_Bias
 tic
