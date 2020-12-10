77b3e421d0da512058f6db8250d45000823a1380
nima2839 <nima2893@gmail.com>
2020-12-10 14:50:47 -0700

    Minor bug fixed

--- Test_Files/Test.m
+++ Test_Files/Test.m
@@ -6,7 +6,7 @@ nangles = [158.0052  147.3529  144.4640  152.8400  139.5369 149.6512  148.7811  
 for i = 1:length(names)
 	cd ~/GRASE/GRASE_To_Do/
 	file = dir(strcat('*', names(i),'*'));
-	UBC_Vs_BSB1(file.name, nangles(i));
+	UBC_Vs_BSB1(file.name(1:end-4), nangles(i));
 	cd ~/GRASE/GRASE_Results/GRASE_B1_Map_Results/
 	file = dir(strcat('*', names(i),'*'));
 	load(file.name,'maps','MWI')
