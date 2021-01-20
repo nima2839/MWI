ac500a906342f984bb140511da1e433503a663a8
nima2839 <nima2893@gmail.com>
2021-01-20 14:30:33 -0700

    fixed variable name

--- Test_Files/Phantom_MEGRE_Sim.m
+++ Test_Files/Phantom_MEGRE_Sim.m
@@ -30,7 +30,7 @@ MyInfo.EchoSpacing = opt.Times(2) - opt.Times(1);
 
 for i = 1:length(SNR)
 	Phantoms{i} = Create_MEGRE_Phantom_4D(MWF, opt);
-	test = TestClass(abs(Phantom{i}), angle(Phantom{i}), MyInfo);
+	test = TestClass(abs(Phantoms{i}), angle(Phantoms{i}), MyInfo);
 	test = Calc_SC(test,2); % LOG method
 	test = Calc_3PM(test);
 	data{i} = GetAllData(test);
