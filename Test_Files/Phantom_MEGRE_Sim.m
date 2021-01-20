3b5fc6eff9cc8fe3f0c61c9500a5bcfeb46ff80e
nima2839 <nima2893@gmail.com>
2021-01-19 19:58:49 -0700

    To test MWI analysis on simlated ME-GRE phantom

---
+++ Test_Files/Phantom_MEGRE_Sim.m
@@ -0,0 +1,39 @@
+% This simulates an MEGRE phantom
+tic;
+disp('Creating 3D phantom...')
+N = 128;
+p = phantom3d('Modified Shepp-Logan', N);
+p(p==0) =nan;
+p(p > 0.25 & p < 0.35) = 0.10;
+p(p > 0.16 & p < 0.25) = 0.05;
+MWF = abs(p(:,:, (floor(N/2) - 15): (floor(N/2) +16)));
+
+temp = zeros(size(MWF));
+temp(MWF == .1) = 15;
+temp(MWF == .05) = -5;
+opt.FreqShift.MW = temp;
+temp(MWF == .1) = 5;
+temp(MWF == .05) = 1;
+opt.FreqShift.IE = temp;
+opt.Times = (1:32) * 2e-3;
+
+disp('Generating 4D MEGRE Phantom...')
+Phantom = Create_MEGRE_Phantom_4D(MWF, opt);
+
+MyInfo.Vox = [1 1 4] * 1e-3;
+MyInfo.Mask = zeros(size(MWF));
+MyInfo.Mask(MWF > 0) = 1;
+MyInfo.FirstTE = opt.Times(1);
+MyInfo.EchoSpacing = opt.Times(2) - opt.Times(1);
+disp('Applying MWI analysis')
+test = TestClass(abs(Phantom), angle(Phantom), MyInfo);
+test = Calc_SC(test,2); % LOG method
+test = Calc_2PM(test);
+test = Calc_3PM(test);
+data = GetAllData(test);
+RunTime = toc;
+
+clear test p MyInfo N
+
+cd ~/Simulation/Phantom/
+save('Phantom_MEGRE_Sim','-v7.3')
