a0b0863b348ed4d783745fa2bd2ae03015137671
nima2839 <nima2893@gmail.com>
2021-02-07 14:19:40 -0700

    This uses Single_Component_T2_Dict to fit for T2 and B1 at the same time

---
+++ Curve_Fitting/Single_Component_T2_B1_Dict.m
@@ -0,0 +1,24 @@
+function [T2, B1, Residual] = Single_Component_T2_B1_Dict(Signal, Dict, T2Range, B1Range)
+% Fits for a parameter using single component fitting using a dictionary matrix and interpolates the range for T2
+% Dict: the dictionary matrix which must be a 2D matrix of decay curves (MxNxETL)
+% B1Range (Nx1)
+% T2Range (Mx1)
+% Signal(ETLx1)
+% Range: range of the parameter used to generate the dictionary matrix 
+	if nargin < 4
+		err('Not enough input arguments!');
+	end
+	
+	%Changing the order of dictionary matrix
+	Dit = permute(Dict, [3,1,2]);
+	res = zeros(1,length(B1Range));
+	T2Vals = res;
+	for i = 1:length(B1Range)
+		temp_dict =  squeeze(Dict(:,:,i));
+		[T2Vals(i),res(i),~] = Single_Component_T2_Dict(Signal, temp_dict, T2Range);
+	end
+	[~,index] = min(res);
+	B1 = B1Range(index);
+	T2 = T2Vals(index);
+	Residual = min(res);
+end
