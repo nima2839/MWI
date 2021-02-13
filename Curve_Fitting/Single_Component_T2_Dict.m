71add659e1de28bfdaf91d43df46bdd78148bd01
nima2839 <nima2893@gmail.com>
2021-02-12 21:37:42 -0700

    Now the fitting method (NNLS/NLLS) can be user defined!

--- Curve_Fitting/Single_Component_T2_Dict.m
+++ Curve_Fitting/Single_Component_T2_Dict.m
@@ -1,11 +1,18 @@
-function [Out, Residual] = Single_Component_T2_Dict(Signal, Dict, Range)
+function [Out, Residual] = Single_Component_T2_Dict(Signal, Dict, Range, Method)
 % Nima: This is currently too slow to use!
 % Fits for a parameter using single component fitting using a dictionary matrix and interpolates the range
 % Dict: the dictionary matrix which must be a 2D matrix of decay curves (ETLxN)
 % Signal(ETLx1)
 % Range: range of the parameter used to generate the dictionary matrix 
+	% define methods
+	NLLS = 1;
+	NNLS = 2;
 	if nargin < 3
 		err('Not enough input arguments!');
+	end
+	
+	if nargin < 4
+		Method = 1;
 	end
 	
 	ETL = length(Signal);
@@ -15,9 +22,14 @@ function [Out, Residual] = Single_Component_T2_Dict(Signal, Dict, Range)
 	% setting options for lsqnonlin function
 	options = optimoptions('lsqnonlin','Algorithm','levenberg-marquardt','TolFun',1e-4,'MaxIter',400,'TolX',1e-4,'Display','off');
 	options.MaxFunEvals = 200;
+	
 	for i = 1:length(Range)
-		Model = @(X)(X* Dict(:,i)) -Signal;
-		[~, res(i)] = lsqnonlin(Model,Signal(1),[],[],options);;
+		if Method == 1
+			Model = @(X)(X* Dict(:,i)) -Signal;
+			[~, res(i)] = lsqnonlin(Model,Signal(1),[],[],options);
+		else
+			[~,res(i),~] = lsqnonneg(Dict(:,i), Signal);
+		end
 	end
 	Range_spline = interp1(Range, 1:.1:numel(Range));
 	res_spline = interp1(Range, res, Range_spline,'spline');
