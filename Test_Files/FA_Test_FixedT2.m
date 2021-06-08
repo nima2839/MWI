edf13cff26483d97585b237fb7c7e606c4669538
nima2839 <nima2893@gmail.com>
2021-06-08 12:44:19 -0600

    Using 150 and 170 for histogram of FA estim results

--- Test_Files/FA_Test_FixedT2.m
+++ Test_Files/FA_Test_FixedT2.m
@@ -3,23 +3,23 @@ addpath ~/Simulation
 addpath ~/MWI
 addpath(genpath('~/GRASE/Postprocessing'))
 
-MyInfo.NumWaterComp = 3;
+MyInfo.NumWaterComp = 2;
 MyInfo.Times = (1:32)*1e-2;
 
-MyInfo.TimeConstRange{1} = [20 20]*1e-3;
-MyInfo.TimeConstRange{2} = [80 80]*1e-3;
-MyInfo.TimeConstRange{3} = [300 300]*1e-3;
-MyInfo.T1Val = [.8 1 1.5];
+MyInfo.TimeConstRange{1} = [15 15]*1e-3;
+MyInfo.TimeConstRange{2} = [75 75]*1e-3;
+%MyInfo.TimeConstRange{3} = [300 300]*1e-3;
+MyInfo.T1Val = [.6 1];
 MyInfo.FractionRange{1}= [0.15,0.15];
-MyInfo.FractionRange{2}= [0.75,0.75];
-MyInfo.FractionRange{3}= [0.1,0.1];
+MyInfo.FractionRange{2}= [0.85,0.85];
+%MyInfo.FractionRange{3}= [0.1,0.1];
 MyInfo.FlipAngle = 180;
 MyInfo.NumData = 500;
 MyInfo.TrueFAFlag = false;
 MyInfo.SNR = 0;
 
-FA = 80:2:180;
-SNR = 30:10:100;
+FA = [150, 170];
+SNR = [100, 500, 1000];
 nFA = length(FA);
 nSNR = length(SNR);
 
@@ -34,9 +34,9 @@ parfor j = 1:nSNR
 		temp.FlipAngle = FA(i)
 		temp.TrueFAFlag = false;
 		a = SimClass(temp);
-		[temp_dist{i},temp_maps{i}] = UBC_Fitting(a);
+		[temp_dist{i},temp_maps{i}] = UBC_Nima_Fitting(a);
 		a.MyInfo.TrueFAFlag = true;
-		[temp_Tdist{i},temp_Tmaps{i}] = UBC_Fitting(a);
+		[temp_Tdist{i},temp_Tmaps{i}] = UBC_Nima_Fitting(a);
 	end
 	Dist{j}{:} = temp_dist;
 	Maps{j}{:} = temp_maps;
@@ -46,4 +46,4 @@ end
 clear a temp
 Description = 'second dim is FlipAngle, and first dim is SNR';
 cd ~/Simulation/Flip_Angle_Test
-save('FA_Result_FixedT2','-v7.3')
+save('FA_Result_FixedT2_150_170','-v7.3')
