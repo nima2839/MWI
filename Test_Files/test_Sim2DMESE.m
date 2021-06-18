af1477199706ad14648e2a0279156e0e0bd06aac
nima2839 <nima2893@gmail.com>
2021-06-18 13:25:04 -0600

    Using smaller spacing in the B1 direction!

--- Test_Files/test_Sim2DMESE.m
+++ Test_Files/test_Sim2DMESE.m
@@ -21,7 +21,7 @@ MyInfo.SeqParams.dx = linspace(-1.5,1.5, 129);
 MyInfo.SeqParams.dz = linspace(-1.5,1.5, 257);
 MyInfo.SeqParams.DSF = 10;
 MyInfo.NumData = 500;
-MyInfo.B1Range = 0.5:.1:1.5;
+MyInfo.B1Range = 0.5:.05:1.5;
 
 MyInfo = rmfield(MyInfo, "LookUpTable");
 %B1_diff = -0.3:.01:.3;
@@ -58,4 +58,4 @@ cd ~/Simulation/MESE2D/
 %	Maps_B1{i}.Residuals = [];
 %	Maps_B1{i}.Distribution = [];
 %end
-save('Sim_2DMESE_B1diffEffect', 'Maps' , 'Maps_B1')
+save('Sim_2DMESE_B1diffEffect')
