8287f7b31ca1c279204a42462d2d726b9f1d7a74
nima2839 <nima2893@gmail.com>
2021-08-16 15:17:53 -0600

    No longer writes batch files for spm; Read_File_Names now accepts file extention as input arg; Now calling spm functions directly within the class;

--- Tools/SPM_Handler.m
+++ Tools/SPM_Handler.m
@@ -14,30 +14,46 @@ classdef SPM_Handler
 			%	'source': Path of source file (jiggled to match the ref)
 			%	'other': Path of the other file that should be coregistered into the same space as source file;
 			%
-			% On Failure out is the excption file, and on success it reurns 0;
 			%
 			%-------------------------------------------------------------------
-			MyJob = ["matlabbatch{1}.spm.spatial.coreg.estwrite.ref = {",...
-					strcat("'",Options.ref, "'};"),...
-					"matlabbatch{1}.spm.spatial.coreg.estwrite.source = {",...
-					strcat("'",Options.source, "'};"),...
-					"matlabbatch{1}.spm.spatial.coreg.estwrite.other = {",...
-					strcat("'",Options.other, "'};"),...
-					"matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.cost_fun = 'nmi';",...
-					"matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.sep = [4 2];",...
-					"matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.tol = [0.02 0.02 0.02 0.001 0.001 0.001 0.01 0.01 0.01 0.001 0.001 0.001];",...
-					"matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.fwhm = [7 7];",...
-					"matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.interp = 4;",...
-					"matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.wrap = [0 0 0];",...
-					"matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.mask = 0;",...
-					"matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.prefix = 'r';"
-					];
+			% Making sure filenames include the '.nii' extension
+			if isempty(strfind(Options.ref,'.nii'))
+				Options.ref = strcat(Options.ref, '.nii');
+			end
+			
+			if isempty(strfind(Options.source,'.nii'))
+				Options.source = strcat(Options.source, '.nii');
+			end
+			
+			if isfield(Options, 'other')
+				if isempty(strfind(Options.other,'.nii'))
+					Options.other = strcat(Options.other, '.nii');
+				end
+			end
+
+			if isempty(which('spm'))
+				error(' Please make sure SPM12 is added to MATLAB path!');
+			end
+
+			matlabbatch{1}.spm.spatial.coreg.estwrite.ref = cellstr(Options.ref);
+			matlabbatch{1}.spm.spatial.coreg.estwrite.source = cellstr(Options.source);
+			if isfield(Options, 'other')
+				matlabbatch{1}.spm.spatial.coreg.estwrite.other = cellstr(Options.other);
+			end
+			matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.cost_fun = 'nmi';
+			matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.sep = [4 2];
+			matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.tol = ...
+							[0.02 0.02 0.02 0.001 0.001 0.001 0.01 0.01 0.01 0.001 0.001 0.001];
+			matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.fwhm = [7 7];
+			matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.interp = 4;
+			matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.wrap = [0 0 0];
+			matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.mask = 0;
+			matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.prefix = 'r';
 			try
-				SPM_Handler.Job_Handler(MyJob);
-				out = 0;
+				spm_jobman('run', matlabbatch);
 			catch ME
 				disp('SPM_Handler.Coregister failed!');
-				out = ME;
+				rethrow(ME);
 			end
 		end
 
@@ -48,9 +64,13 @@ classdef SPM_Handler
 			%	'OutDir': output directory, if not provided it would write into the current directory;
 			%	'Num_Of_Files': If provided, will only read n first DICOM files, otherwise it will read all;
 			%	'Name': name of output NIFTI file
-			% On Failure out is the excption file, and on success it reurns 0;
+			% On success returns a cell containing the name of the written files;
 			%
 			%-------------------------------------------------------------------
+			if isempty(which('spm'))
+				error(' Please make sure SPM12 is added to MATLAB path!');
+			end
+
 			if ~isfield(Options, 'Num_Of_Files')
 				Options.Num_Of_Files = 0;
 			end
@@ -59,90 +79,56 @@ classdef SPM_Handler
 				Options.OutDir = pwd;
 			end
 
-
-			MyJob = ["matlabbatch{1}.spm.util.import.dicom.data = {",...
-					SPM_Handler.Read_File_Names(Options.ReadPath, Options.Num_Of_Files),...
-					"};",...
-					"matlabbatch{1}.spm.util.import.dicom.root = 'flat';",...
-					"matlabbatch{1}.spm.util.import.dicom.outdir = {", strcat("'",Options.OutDir,"'") ,...
-					"};",...
-					"matlabbatch{1}.spm.util.import.dicom.protfilter = '.*';",...
-					"matlabbatch{1}.spm.util.import.dicom.convopts.format = 'nii';",...
-					"matlabbatch{1}.spm.util.import.dicom.convopts.meta = 0;",...
-					"matlabbatch{1}.spm.util.import.dicom.convopts.icedims = 0;"
-					];
-
 			try
-				SPM_Handler.Job_Handler(MyJob);
-				out = 0;
-				% renaming the output file
+				Headers =  spm_dicom_headers(SPM_Handler.Read_File_Names(Options.ReadPath, Options.Num_Of_Files));
+				spm_output = spm_dicom_convert(Headers,'all','flat','nii', Options.OutDir);
+				% renaming the output files
 				if isfield(Options, 'Name')
-					cd(Options.OutDir)
-					file = dir('s*.nii');
-					if length(file) > 1
-						[~,idx] = sort([file.datenum]);
-						file = file(idx(end));
+					% cd(Options.OutDir)
+					n =  numel(spm_output.files);
+					ext = "";
+					out = cell(n, 1);
+					for i = 1:n
+						if n > 1
+							ext = strcat("_Echo_", string(i));
+						end
+						out{i} = strcat(Options.OutDir,'\', Options.Name, ext, ".nii");
+						movefile(spm_output.files{i}, out{i});
 					end
-					movefile(file.name, strcat(Options.Name, ".nii"));
+				else
+					out  = spm_output.files;
 				end
 			catch ME
 				disp('SPM_Handler.DICOM_TO_NIFTI failed!');
-				out = ME;
+				rethrow(ME);
 			end
 		end
 
-		function FileNames = Read_File_Names(ReadDir, n)
+		function FileNames = Read_File_Names(ReadDir, n, ext)
 			% Reads file names to add to use in Job file
-			% n input is the number of files in directory
-			FileNames = "";
-			files = dir(strcat(ReadDir,'/*.dcm'));
-			if n == 0
-				n = length(files);
-			end
-
-			for i = 1:n
-				FileNames = [FileNames, strcat("'",ReadDir, "/", files(i).name, "'")];
-			end
-		end
-
-		function Job_Handler(Job)
-			% Writes down "Job" input into a file and then runs spm12 code
-			% Job is a string array in the same format as spm12 scripts
+			% 'n' is the number of files in directory
+			% 'ext' is the extention of the files
 			%
 			%-------------------------------------------------------------------
-			% Creating a new file (or clear file) to write the job into
-			FileName = 'temp_SPM12_Job.m';
-			Header = "% This file is generated by SPM_Handler object!";
-			SPM_Handler.Write_To_File(FileName, Header, 'wt');
 
-
-			% Writing Job into file
-			SPM_Handler.Write_To_File(FileName, Job, 'at');
-
-			% Runnig the Job on SPM12
-			spm('defaults', 'FMRI');
-			spm_jobman('run', [pwd, '/', FileName]);
-
-			% clearing Job file
-			delete(FileName);
-		end
-
-		function Write_To_File(FileName, text, permission)
-			% Writes text into file and throws excption on failure
-			FID = fopen(FileName, permission);
-
-			if FID == -1
-				ME = MException('MyComponent:fopenFailed',['MATLAB could not open ', FileName, '!']);
-				throw(ME);
+			if nargin < 3
+				ext = '.dcm';
 			end
 
-			if fprintf(FID,'%s\n', text)  == 0
-				ME =  MException('MyComponent:fprintf_Failure',...
-						'MATLAB failed to write in job file!');
-				fclose(FID);
-				throw(ME);
+			FileNames = "";
+			files = dir(strcat(ReadDir, '/*', ext));
+			
+			if nargin < 2
+				n = 0;
 			end
-			fclose(FID);
+
+			if n == 0 
+				n = length(files);
+			end
+			FileNames = cell(1,n);
+			for i = 1:n
+				FileNames{i} = char(strcat(ReadDir, "/", files(i).name));
+			end
 		end
 
 	end
