08358d15f142628f3364728f3227fe2b2686e100
nima2839 <nima2893@gmail.com>
2021-08-17 13:12:30 -0600

    Added "Find_Mask" and "Find_NominalAngle" fields which are function pointers that can be decorated!

--- Test_Files/ProcessGRASE.m
+++ Test_Files/ProcessGRASE.m
@@ -1,60 +1,91 @@
-function  ProcessGRASE(Save_Name, MyInfo)
+function  Results = ProcessGRASE(Options, Save_Name)
 % Processes GRASE MWI directly from DICOM files;
 % Requires '\MWI' and '\spm12' to be added to the working path;
-% Estimated flip angle method will always be performed;
-% Supplied flip angle method is only performed when dirctory to DICOM files of the B1 phase maps are specified; 
-% A "Description" is saved along with the result in the case of using a flip angle map
 %
-% "Save_Name": name of the file that will be saved;
-% "MyInfo" is a structure containing the following fields:
-%   - 'GRASE_Dir': path to GRASE DICOM files;
-%   - 'Save_Dir': path to where the data should be saved; (optional)
-%   - 'B1Phase_Dir': path to B1 phase DICOM files (used to aquire flip angle map); (optional)
-%   - 'B1Mag_Dir': path to B1 magnitude DICOM files (used for coregisteration purposes); (optional)
-%   - 'B1_Scale': the scaling factor required to noramalize B1 Phase data (800); (optional) 
-%   - 'Nominal_Angle': The scaling factor to aquire flip angle map from the normalized B1 map; (optional but crucial)
+% Supplied flip angle method is only performed when dirctory to DICOM files of the B1 phase maps are specified; 
+% A "Description" is retured along with the result in the case of using a flip angle map
+%
+% Output:
+% "Results" is a structure containing all results;
+%
+% Input arguments:
+% "Save_Name" if provided it would save results under the given name; (optional)
+% "Options" is a structure containing the following fields:
+%   - 'GRASE_Dir' is the path to GRASE DICOM files;
+%   - 'Save_Dir' is the path to where the data should be saved; (optional)
+%   - 'B1Phase_Dir' is the path to B1 phase DICOM files (used to aquire flip angle map); (optional)
+%   - 'B1Mag_Dir' is the path to B1 magnitude DICOM files (used for coregisteration purposes); (optional)
+%   - 'B1_Scale' is the scaling factor required to noramalize B1 Phase data (800); (optional) 
+%   - 'Nominal_Angle' is the scaling factor to aquire flip angle map from the normalized B1 map; (optional)
+%   - 'Find_Mask' is a function pointer that user specifies to find mask using only the 4D magnitude data
+%       + Ex: Options.Find_Mask = @(Mag) Create_Mask(Mag(:,:,:,1), 0.35); % "Create_Mask" can be found under "\MWI\Tools\"
+%       + (optional)
+%   - 'Find_NominalAngle' is a function pointer that accepts two input args: (B1, Estimated_Flip_Angle_Map)
+%       + Where 'B1' is the normalized coregistered B1 Phase map, and 'Estimated_Flip_Angle_Map' is the estimation of the 
+%         refocusing angles using the normal analysis (handled by this funciton);
+%       + (optional)
 %
   % Checking and intializing the input
-  if ~isfield(MyInfo, 'GRASE_Dir')
+  if ~isfield(Options, 'GRASE_Dir')
     error("GRASE_Dir is required!")
   end
 
-  if isfield(MyInfo, 'Save_Dir')
-    cd(MyInfo.Save_Dir);
+  if isfield(Options, 'Save_Dir')
+    cd(Options.Save_Dir);
   end
 
-  if ~isfield(MyInfo, 'B1_Scale')
-    MyInfo.B1_Scale = 800;
+  if ~isfield(Options, 'B1_Scale')
+    Options.B1_Scale = 800;
   end
 
-  if ~isfield(MyInfo, 'Nominal_Angle')
-    MyInfo.Nominal_Angle = 156.5; % calculated empirically
+  if ~isfield(Options, 'Nominal_Angle')
+    if isempty(Options, 'Nominal_Angle') 
+      if ~isfield(Options, 'Find_NominalAngle')
+        Options.Nominal_Angle = 156.5; % calculated empirically
+      end
+    end
   end
 
   FlipAngleMap = [];
   % Reading GRASE images along with proper information for NIFTI format
-  [Image, nifti_info] = ReadAllDCM(MyInfo.GRASE_Dir, 'spm');
+  [Image, nifti_info] = ReadAllDCM(Options.GRASE_Dir, 'spm');
+
+  if isfield(Options, 'Find_Mask')
+    try
+      Mask = Options.Find_Mask(Image);
+      sd = size(Image);
+      temp = reshape(Image, [numel(Mask), sd(4)]);
+      idx = find(Mask > 0);
+      temp(idx,:) = 0;
+      Image = reshape(temp, sd);
+    catch ME
+      disp(ME);
+      disp('Masking process failed!');
+    end
+  end
+
+
   
-  if isfield(MyInfo, 'B1Phase_Dir')
+  if isfield(Options, 'B1Phase_Dir')
     % reading dicom files for coregisteration
-    opt.ReadPath = MyInfo.B1Phase_Dir;
+    opt.ReadPath = Options.B1Phase_Dir;
     opt.Name = strcat('B1_Phase_temp', date)
     temp_files{1} = opt.Name;
     SPM_Handler.DICOM_TO_NIFTI(opt);
-    if isfield(MyInfo, 'B1Mag_Dir')
-      opt.ReadPath = MyInfo.B1Mag_Dir;
+    if isfield(Options, 'B1Mag_Dir')
+      opt.ReadPath = Options.B1Mag_Dir;
       opt.Name = strcat('B1_Mag_temp', date)
       temp_files{2} = opt.Name;
       SPM_Handler.DICOM_TO_NIFTI(opt);
     end
-    opt.ReadPath = MyInfo.GRASE_Dir;
+    opt.ReadPath = Options.GRASE_Dir;
     opt.Name = strcat('GRASE_Mag_temp', date)
     temp_files{numel(temp_files) + 1} = opt.Name;
     opt.Num_Of_Files = size(Image, 3);
     SPM_Handler.DICOM_TO_NIFTI(opt);
     % coregisteration part
     opt.ref = strcat(temp_files{end}, '.nii');
-    if isfield(MyInfo, 'B1Mag_Dir')
+    if isfield(Options, 'B1Mag_Dir')
       temp_files{4} = strcat('r', temp_files{1});
       temp_files{5} = strcat('r', temp_files{2});
       opt.source = strcat(temp_files{2}, '.nii');
@@ -64,7 +95,7 @@ function  ProcessGRASE(Save_Name, MyInfo)
       opt.source = temp_files{1};
     end
     SPM_Handler.Coregister(opt);
-    FlipAngleMap = MyInfo.Nominal_Angle * double(niftiread(strcat('r',temp_files{1},'.nii'))) / MyInfo.B1_Scale;
+    FlipAngleMap = Options.Nominal_Angle * double(niftiread(strcat('r',temp_files{1},'.nii'))) / Options.B1_Scale;
     % clearing the temp files
     for i = 1:numel(temp_files)
       delete(strcat(temp_files{i}, '.nii'));
@@ -73,12 +104,28 @@ function  ProcessGRASE(Save_Name, MyInfo)
   
   % MWI analysis
   [Maps, Dist, ~] = T2map_Nima(Image);
-  if isempty(FlipAngleMap)
-    save(Save_Name, 'Maps', 'Dist', 'nifti_info');
-  else
-    [Maps_B1, Dist_B1, ~] = T2map_Nima(Image, 'FlipAngleMap', FlipAngleMap);
-    Description = "The '_B1' extention specifies the supplied B1 method resutls";
-    save(Save_Name, 'Maps', 'Dist', 'Maps_B1', 'Dist_B1', 'Description','nifti_info');
+  Results.Maps = Maps;
+  Results.Dist = Dist;
+  Results.nifti_info = nifti_info
+
+  if ~isfield(Options, 'Find_NominalAngle')
+    try 
+      FlipAngleMap = Options.Find_NominalAngle(Image, Maps.alpha);
+    catch ME
+      disp(ME);
+      disp('Nominal Angle estimation failed! Skipping B1 supplied method!');
+      FlipAngleMap = [];
+    end
   end
 
+  if ~isempty(FlipAngleMap)
+    [Maps_B1, Dist_B1, ~] = T2map_Nima(Image, 'FlipAngleMap', FlipAngleMap);
+    Results.Description = "The '_B1' extention specifies the supplied B1 method resutls";
+    Results.Maps_B1 = Maps_B1;
+    Results.Dist_B1 = Dist_B1;
+  end
+
+  if nargin > 1
+    save(Save_Name, 'Results');
+  end
 end
